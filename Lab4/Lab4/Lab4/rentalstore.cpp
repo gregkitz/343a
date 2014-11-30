603f7f5285106cba9a4b1980b5469bd9af5fb415
Greg <greg.kitzmiller@gmail.com>
2014-11-30 14:18:50 -0800

    more..

--- Lab4/Lab4/Lab4/rentalstore.cpp
+++ Lab4/Lab4/Lab4/rentalstore.cpp
@@ -3,10 +3,42 @@
 #include <fstream> 
 using namespace std; 
 
-const string customerInfile = "data4customers.txt"; 
+bool RentalStore::processMovies(ifstream& infile){
+	//try and open the file 
+	if (!infile) {
+	cout << "File could not be opened." << endl;
+	return 1;    //1 is failure condition 
+	}
+	//if it can, read the first character to see what type to be processed
+	char type; 
+	infile >> type;    // might read into strings and do strcmp instead..
+	if (type == 'F'){
+		Comedy * tempComedy = createComedy(int, string, string, string, int); //create it 
+				//insert it into the comedy BST
+	}
 
-bool RentalStore::initializeCustomers(){
-	ifstream customersInfile(customerInfile); //associate the stream with the customer infile 
-	theCustomers.
+	else if (type == 'D'){
+		Drama* tempDrama = createDrama(int, string, string, string, int); 
+	}
+
+	else if (type == 'C'){
+		Classic* tempClassic = createClassic(int, string, string, string, string, string, int, int);
+
+	}
+
+		//else condition handles invalid data 
+	else {
+
+	}
+
+}
+
+Comedy* RentalStore::createComedy(int, string, string, string, int){
+
+}
+Drama* RentalStore::createDrama(int, string, string, string, int){
+
+}
+Classic* RentalStore::createClassic(int, string, string, string, string, string, int, int){
 
 }
--- Lab4/Lab4/Lab4/rentalstore.h
+++ Lab4/Lab4/Lab4/rentalstore.h
@@ -1,13 +1,21 @@
+//Manager class
+//Contains three BST's for movies, a hash table for customers, and a queue of commands 
+
 #ifndef _RENTALSTORE_H
 #define _RENTALSTORE_H
 #include "customers.h"
-
+#include "comedy.h"
+#include "drama.h"
+#include "classics.h"
 
 class RentalStore{
 public: 
 	bool initializeCustomers();
-
-
+    bool processMovies(ifstream&); // builds movies from a file and puts them in the hash table 
+    Comedy* createComedy(int, string, string, string, int); 
+    Drama* createDrama(int, string, string, string, int); 
+    Classic* createClassic(int, string, string, string, string, string, int, int); 
+    bool handleInvalidData(); // for unrecognized characters from the first thing read in 
 private: 
 	Customers theCustomers; 
 
